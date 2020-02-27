98d8b3bc32a7e091464ac9386e31a3af69cece6a
zawadka <louis.78100@hotmail.fr>
2020-02-27 15:39:54 +0100

    Update Fixtures

--- src/DataFixtures/LinksFixtures.php
+++ src/DataFixtures/LinksFixtures.php
@@ -10,6 +10,26 @@ class LinksFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
+      $categorie = [
+        'déchets',
+        'déchets'
+      ];
+
+      $description = [
+        'Voici une solution pour vous permettre de ne pas jeter de vos objets, vous pouvez tout simplement les faire réparer !',
+        'Regarder cette vidéo qui explique comment trier ces déchets'
+      ];
+
+      $link = [
+        'https://fr.ifixit.com/Tutoriel',
+        'https://www.youtube.com/watch?v=rNwtMO_Hay4'
+      ];
+
+      $name = [
+        'repare',
+        'tri'
+      ];
+
       $user = (new Links())
         ->setCategorie('déchets')
         ->setDescription('Voici une solution pour vous permettre de ne pas jeter de vos objets, vous pouvez tout simplement les faire réparer !')
